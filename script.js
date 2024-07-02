6af035f4a2f004a7e60736dd940fa47da098acac
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-02 19:56:04 +1000

    Simple layout and number button to screen
    Created basic calculator CSS structure for visuals.
    Function to store button presses and display
    to screen.

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ function divide(num1,num2){
     return num1/num2;
 }
 
-let input1, input2, operator;
-
 function operate(input1,input2,operator){
     switch(operator){
         case "+": return add(input1,input2);
@@ -25,3 +23,25 @@ function operate(input1,input2,operator){
     }
 
 }
+
+function buttonClick(eventObj){
+    let input = eventObj.target.textContent;
+    if(eventObj.target.classList.contains("number")){
+        displayValue.push(+input);
+    }
+    updateScreen(displayValue,screen);
+}
+
+function updateScreen(displayValue, screen){
+    if(screen.textContent === 0){ 
+        screen.textContent = ''
+    }
+    screen.textContent = displayValue.join('');
+}
+
+let input1, input2, operator;
+let displayValue =[];
+
+const buttons = document.querySelector(".buttons");
+const screen = document.querySelector(".screen");
+buttons.addEventListener("click",buttonClick);
