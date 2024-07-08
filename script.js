acea663e8b25b1ce229730e73153dd1df73aaf24
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-08 13:24:40 +1000

    Basic equation function
    Function to input 2 numbers and an operator.
    Results is printed to screen.
    
    Lacks error checking.

--- script.js
+++ script.js
@@ -18,23 +18,59 @@ function operate(input1,input2,operator){
     switch(operator){
         case "+": return add(input1,input2);
         case "-": return subtract(input1,input2);
-        case "*": return multiply(input1,input2);
-        case "/": return divide(input1,input2);
+        case "x": return multiply(input1,input2);
+        case "÷": return divide(input1,input2);
     }
-
 }
 
 function buttonClick(eventObj){
-    let input = eventObj.target.textContent;
-    if(eventObj.target.classList.contains("number")){
-        displayValue.push(+input);
+    let input = eventObj.target;
+    if(input.classList.contains("number")){
+        displayValue.push(+input.textContent);
+        updateScreen(displayValue,screen);
     }
-    updateScreen(displayValue,screen);
+    else if (input.classList.contains("operator")){
+        if(!input1){
+            input1 = displayValue.join('');
+            displayValue.splice(0);
+        }
+        else if (!input2){
+            input2 = displayValue.join('');
+            displayValue.splice(0);
+        } 
+        /*
+        else{
+            console.log(`Input 1: ${input1}`);
+            let temp = operate(input1,input2,operator);
+            console.log(`Temp Value: ${temp} Type of Temp: ${typeof(temp)}`);
+            input1 = temp;
+            console.log(`Input 1: ${input1}`);
+            input2 = 0;
+            displayValue.splice(0);
+        }
+        */
+        /*
+        else{
+            input2 = displayValue.join('');
+            displayValue.splice(0);
+        }   */
+        operator = input.textContent;
+        updateScreen([0],screen);
+        console.log(input1);
+        console.log(operator);
+        console.log(input2);
+    }
+    else if (input.classList.contains("equals")){
+        input2 = displayValue.join('');
+        displayValue.splice(0);
+        updateScreen([operate(input1,input2,operator)],screen);
+    }
+    
 }
 
 function updateScreen(displayValue, screen){
     if(screen.textContent === 0){ 
-        screen.textContent = ''
+        screen.textContent = '';
     }
     screen.textContent = displayValue.join('');
 }
