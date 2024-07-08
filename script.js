29739fdcbd503f45765c5a52610c4ba2164d2da2
dtamb <89025060+dtamb@users.noreply.github.com>
2024-07-08 14:07:36 +1000

    Consecutive operations

--- script.js
+++ script.js
@@ -29,41 +29,31 @@ function buttonClick(eventObj){
         displayValue.push(+input.textContent);
         updateScreen(displayValue,screen);
     }
+    // else if(input.classList.contains("point")){
+    //     if()
+    // }
     else if (input.classList.contains("operator")){
         if(!input1){
             input1 = displayValue.join('');
             displayValue.splice(0);
+            updateScreen([0],screen);
         }
         else if (!input2){
+            console.log(typeof(input1));
             input2 = displayValue.join('');
             displayValue.splice(0);
-        } 
-        /*
-        else{
-            console.log(`Input 1: ${input1}`);
-            let temp = operate(input1,input2,operator);
-            console.log(`Temp Value: ${temp} Type of Temp: ${typeof(temp)}`);
-            input1 = temp;
-            console.log(`Input 1: ${input1}`);
+            input1 = operate(+input1,+input2,operator);
             input2 = 0;
-            displayValue.splice(0);
-        }
-        */
-        /*
-        else{
-            input2 = displayValue.join('');
-            displayValue.splice(0);
-        }   */
+            updateScreen([input1],screen);
+        } 
         operator = input.textContent;
-        updateScreen([0],screen);
-        console.log(input1);
-        console.log(operator);
-        console.log(input2);
     }
     else if (input.classList.contains("equals")){
         input2 = displayValue.join('');
         displayValue.splice(0);
-        updateScreen([operate(input1,input2,operator)],screen);
+        updateScreen([operate(+input1,+input2,operator)],screen);
+        input1 = 0;
+        input2 = 0;
     }
     
 }
